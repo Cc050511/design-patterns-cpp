4457242abb668f2bf63bcc9333a38f19d1948025
Cc050511 <94614996+Cc050511@users.noreply.github.com>
2026-06-13 14:31:30 +0800

    revert: remove singleton implementation to restore template
    Keep evaluation checklist but remove the completed implementation
    so users can implement it themselves.

--- src/creational/singleton/main.cpp
+++ src/creational/singleton/main.cpp
@@ -38,40 +38,9 @@
 
 #include <iostream>
 
-class Singleton {
-  public:
-    static Singleton& instance() {
-        static Singleton instance;
-        return instance;
-    }
-
-    Singleton(const Singleton&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
-    Singleton(Singleton&&) = delete;
-    Singleton& operator=(Singleton&&) = delete;
-
-    void operation() const {
-        std::cout << "Singleton instance address: " << this << "\n";
-    }
-
-  private:
-    Singleton() {
-        std::cout << "Singleton constructed (lazy init)\n";
-    }
-    ~Singleton() = default;
-};
+// TODO: 实现你的 Singleton
 
 int main() {
     std::cout << "=== Singleton Demo ===\n";
-    std::cout << "Getting instance 1...\n";
-    Singleton& s1 = Singleton::instance();
-    s1.operation();
-
-    std::cout << "Getting instance 2...\n";
-    Singleton& s2 = Singleton::instance();
-    s2.operation();
-
-    std::cout << "Same instance? " << (&s1 == &s2 ? "YES" : "NO") << "\n";
-    std::cout << "Singleton verified successfully.\n";
-    return 0;
+    // TODO
 }
